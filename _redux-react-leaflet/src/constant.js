3c50a00db886cae05e250abc97754cb92edfa765
Michele Ferretti <mic.ferretti@gmail.com>
2017-12-26 17:58:31 +0100

    rm old folders

--- _redux-react-leaflet/src/constant.js
+++
@@ -1,1 +0,0 @@
-export const SET_NEW_CENTER = "SET_NEW_CENTER";
--- redux-react-leaflet-app-copy/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./containers/App/App.js";
-import registerServiceWorker from "./registerServiceWorker";
-// import "./reducers.js";
-
-ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();

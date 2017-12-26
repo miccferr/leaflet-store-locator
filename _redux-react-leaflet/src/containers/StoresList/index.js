9ab770e6dfe4f4c9fb8d8a329b61224934ee6640
Michele Ferretti <mic.ferretti@gmail.com>
2017-12-26 17:37:40 +0100

    refactore store list component

--- _redux-react-leaflet/src/containers/StoresList/index.js
+++ _redux-react-leaflet/src/containers/StoresList/index.js
@@ -1,13 +1,34 @@
 import React, { Component } from "react";
 import "../../../../node_modules/bulma/sass/utilities/_all.sass";
 import "../../../../node_modules/bulma/sass/grid/columns.sass";
-import StoreItems from "../../components/StoreItems";
-
+import StoreItem from "../../components/StoreItem";
+const listStyle = { listStyle: "none" };
 class StoreList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { whichIsSelected: null };
+  }
+  clickHandler(idx) {
+    console.log("CLICCC", idx);
+    this.setState({ whichIsSelected: idx });
+  }
   render() {
+    const StoreItems = this.props.data.features.map((e, i) => {
+      var is_selected = this.state.whichIsSelected == i;
+      return (
+        <StoreItem
+          key={i}
+          setCenter={this.props.setCenter}
+          address={e.properties.address}
+          clickHandler={() => this.clickHandler}
+          index={i}
+          isSelected={is_selected}
+        />
+      );
+    });
     return (
       <div className="menu-scroll">
-        <StoreItems setCenter={this.props.setCenter} data={this.props.data} />
+        <ul style={listStyle}>{StoreItems} </ul>
       </div>
     );
   }
